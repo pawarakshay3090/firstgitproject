8d097943411f08499b05359f76a746b6e875ef24
Akshay Pawar <akshaypawar@gmail.com>
2022-08-23 11:58:34 +0530

    commit 1

---
+++ firstgit/src/firstgit_demo/sample1.java
@@ -0,0 +1,23 @@
+package firstgit_demo;
+
+public class sample1 {
+	public static void main(String[] args) {
+		
+		
+		
+		
+		System.out.println("first code");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
